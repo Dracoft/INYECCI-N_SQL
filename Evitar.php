73f9a716eb2afd6f0c5645ea699d00b6e6955225
Incógnita X <julian.cortes43@gmail.com>
2020-10-22 14:31:38 -0500

    Create Evitar.php

---
+++ Evitar.php
@@ -0,0 +1,12 @@
+<?php
+$user = $conn->real_escape_string($_POST['user']);
+$password = $conn->real_escape_string($_POST['password']);
+
+$user = addslashes($_POST['user']);
+session_regenerate_id(true);
+$password = addslashes($_POST['password']);
+
+<input type="text" class="form-control" pattern="[A-Za-z0-9_-]{1,20}" name="user" placeholder=" Usuario" required>
+<input type="password" class="form-control" pattern="[A-Za-z0-9_-]{1,20}" name="password" placeholder=" Contraseña" required>
+
+?>
